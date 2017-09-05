ed888eb8b6686a437b04e6c9a353a742c8901882
dilipthakor <thakordilip8@gmail.com>
2017-09-05 22:04:03 +0530

    2 html templete delet and write node js file

--- server.js
+++ server.js
@@ -5,29 +5,89 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles ={
+
+'artical-one': {
+	title: 'Artical One : Dilip Thakor',
+	heading: 'Artical One',
+	date:'5 sep, 2017',
+	content:`
+	<p>
+	rtical is about me.
+	  </p>`
+	},
+'artical-two': {
+	title: 'Artical Two : RD worman',
+	heading: 'Artical three',
+	date:'25 sep, 2017',
+	content: `
+	<p>
+	    his arti hangout with me
+	  </p>`
+	},
+'artical-three': {
+	title: 'Artical Two : VD vadanaadr',
+	heading: 'Artical Two',
+	date:'10 sep, 2017',
+	content: ` 
+	<p>
+	     its me. boring !!!! lol. ture i am a boring person if hangout with me.This artical is about me. yes its me. boring !!!! lol. ture i am a boring person if hangout with meThis artical is about me. yes its me. boring !!!! lol. ture i am a boring person if hangout with meThis artical is about me. yes its me. boring !!!! lol. ture i am a boring person if hangout with me
+	  </p>`
+	}
+
+};
+	
+	
+
+function createTemplate(data){	
+  var title=data.title;
+  var date=data.date;
+  var heading= data.heading;
+  var content= data.content;
+  var htmlTemplate=`
+  <html>
+  <head>
+    <title>
+    ${title}
+    </title>
+    <meta name=""viewport content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+  <body>
+	 <div class="container">
+    <div>
+    <a href="/">Home</a>
+    </div>
+    <hr/>
+    <div>
+    <h3>
+    ${heading}
+    </h3>
+    </div>
+    ${date}
+    <div>
+	  ${content}
+	</div>
+	</div>
+  </body>
+</html>
+
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/artical-one', function(req, res){
-	res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+app.get('/:articleName', function(req, res){
+	//articalName == aritcal-one
+	var articleName = req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
 	});
 
-app.get('/artical-two', function(req, res){
-	res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
-	});
-	
-app.get('/artical-three', function(req, res){
-	res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
-	});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
