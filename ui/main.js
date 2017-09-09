377e21bb84b4bb361b973b895923325ab04fbd45
dilipthakor <thakordilip8@gmail.com>
2017-09-10 01:52:22 +0530

    api counter request

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,7 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
-var img = document.getElementById('madi');
+/*var img = document.getElementById('madi');
 
-var marginLeft = 0;
 
  function moveRight(){
   marginLeft = marginLeft +'1';
@@ -11,5 +10,33 @@ var marginLeft = 0;
 };
 img.onclick = function(){
   var interval = setInterval(moveRight,10);
-  //img.style.marginLeft = "100px";
+  img.style.marginLeft = "100px";
+};
+
+*/
+
+
+
+var button = document.getElementById('counter');
+
+
+button.onclick = function(){
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function(){
+
+	if(request.readystate === XMLHttpRequest.DONE){
+
+		if(request.status === 200){
+			var counter = request.responseText;
+			var span = document.getElementById('count');
+			span.innerHTML = counter.toString();
+		}
+	}
+};
+
+	request.open('GET', 'http://localhost:8080/counter', true);
+	request.send(null);	
+
 };
